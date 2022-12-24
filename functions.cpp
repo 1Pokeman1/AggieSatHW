bbdf50232d9f6b3aaa2336d38822aa2adf096c39
Luke Hopkins <lukehopkins30@gmail.com>
2022-12-24 15:19:24 -0600

    added information on const and pass by reference

--- functions.cpp
+++ functions.cpp
@@ -21,12 +21,28 @@ void print_num(int num) {
     cout << num << endl;
 }
 
+// the & after the parameter type tells the function to literally use the same variable that was passed in without making a copy
+// known as pass by reference
+void add_one(int& num) {
+    num += 1;
+}
+
+// const tells the compiler that the function has no intention of changing the variable which can help both the programmer and compiler write better code
+void cprint_num(const int num) {
+    cout << num << endl;
+}
+
 int main() {
     int a = 1, b = 3;
     int c = add(a, b); //note: the variables don't need to be called the same thing as the function definition
     print_num(a);
     print_num(b);
     print_num(c);
+
+    // adds one to c b/c c is passed as a reference
+    add_one(c);
+    // const b/c c is not being changed
+    cprint_num(c);
 }
 
 // this is the definition for `add`
